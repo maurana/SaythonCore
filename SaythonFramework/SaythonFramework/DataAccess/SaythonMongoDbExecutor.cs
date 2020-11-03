085aae2069a41b4725dab4fe084cf9ff540b3771
maurana <maurana.hasegawa@gmail.com>
2020-11-03 20:50:06 +0700

    Update

---
+++ SaythonFramework/SaythonFramework/DataAccess/SaythonMongoDbExecutor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MongoDB.Driver;
+
+/// <summary>
+/// Saython MongoDB Executor Class
+/// </summary>
+namespace SaythonFramework.DataAccess
+{
+    public class SaythonMongoDbExecutor
+    {
+        public string ConnectionString;
+        MongoClient _connection;
+        CommandDocument _command;
+
+        public SaythonMongoDbExecutor(string conString)
+        {
+            this.ConnectionString = conString;
+        }
+
+        public MongoClient Connection
+        {
+            get
+            {
+                if (_connection == null)
+                {
+                    if (ConnectionString == "")
+                        throw new Exception("Connection string must be defined before using connection, command or data adapter");
+
+                    _connection = new MongoClient(ConnectionString);
+                }
+
+                return _connection;
+            }
+            set
+            {
+                _connection = value;
+            }
+        }
+
+        public CommandDocument Command
+        {
+            get
+            {
+                if (_command == null)
+                {
+                    _command = new CommandDocument();
+                }
+
+                return _command;
+            }
+            set
+            {
+                _command = value;
+            }
+        }
+    }
+}
